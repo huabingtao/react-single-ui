296c9d557bb1830c5e02229f5208fb54ef6be43d
huabingtao <1273621932@qq.com>
2021-05-19 22:55:40 +0800

    build 修改配置文件打包

--- .umirc.ts
+++ .umirc.ts
@@ -1,19 +1,20 @@
 /*
  * @Author: your name
  * @Date: 2021-05-14 23:10:58
- * @LastEditTime: 2021-05-19 22:33:18
- * @LastEditors: your name
+ * @LastEditTime: 2021-05-19 22:54:46
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /single/.umirc.ts
  */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'single',
+  title: 'single-ui',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  base: 'single-ui',
   // more config: https://d.umijs.org/config
 });
