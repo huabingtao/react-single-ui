096f17e12cb86c6edf927d459759bbe297568e88
huabingtao <1273621932@qq.com>
2021-05-19 23:16:26 +0800

    build 删除配置文件base属性

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-05-14 23:10:58
- * @LastEditTime: 2021-05-19 23:06:06
+ * @LastEditTime: 2021-05-19 23:13:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /single/.umirc.ts
@@ -15,7 +15,6 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  base: 'single-ui',
   publicPath: '/single-ui/',
   // more config: https://d.umijs.org/config
 });
