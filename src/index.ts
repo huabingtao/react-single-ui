46c8dfc07e3b545a4d742716329ec7ea57c330b8
huabingtao <1273621932@qq.com>
2021-05-20 16:15:21 +0800

    build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import 'antd-mobile/dist/antd-mobile.less';
 import './style/index.less';
 export { default as Loading } from './base/loading';
 export { default as Icon } from './base/icon';
-export { default as Modal } from './base/modal';
