49cdb05d719f8300708084b85810ecbbdfff4b25
Abing <1273621932@qq.com>
2024-09-14 15:46:26 +0800

    style: setting prettier eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 // .eslintrc.js
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true, // 如果你在浏览器环境中工作
     node: true, // 如果你在 Node.js 环境中工作
     es2020: true, // 支持 ECMAScript 2020
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // 启用 Prettier，并且关闭与 Prettier 冲突的 ESLint 规则
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   rules: {
+    // 配置 Prettier 的规则
+    'prettier/prettier': 'error',
     '@typescript-eslint/no-unused-expressions': [
       'error',
       { allowShortCircuit: true },
